9549fa912b5d8084bc47876e8ad131a05579ce69
AlreadyHaveAnAcc <bwabersmanschool@gmail.com>
2022-08-26 23:33:24 -0700

    login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { FaSignInAlt } from "react-icons/fa";
 
 interface Props {
 	links: {
@@ -9,7 +10,7 @@ interface Props {
 }
 
 const Navbar: React.FC<Props> = ({ links }): JSX.Element => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 	return (
 		<nav className="flex items-center justify-around px-3 mt-1 md:justify-between h-14 sm:px-10">
 			<div className="flex items-center h-20 py-6 cursor-pointer">
@@ -18,16 +19,21 @@ const Navbar: React.FC<Props> = ({ links }): JSX.Element => {
 					alt="Keep"
 					className="w-full h-full"
 				/> */}
-				<p className="text-2xl font-bold whitespace-nowrap">Super Speller</p>
+				<p
+					className="text-2xl font-bold whitespace-nowrap"
+					onClick={() => navigate("/")}
+				>
+					Super Speller
+				</p>
 			</div>
 			<div className="flex items-center justify-end w-1/3 h-full md:w-2/3">
 				<div className="flex items-center justify-center mr-20 scale-0 lg:scale-100">
 					{links.map((navItem, index) => {
 						return (
-							<div 
-                                className="flex flex-col items-center justify-center h-full transition-colors cursor-pointer group"
+							<div
+								className="flex flex-col items-center justify-center h-full transition-colors cursor-pointer group"
 								onClick={() => navigate(navItem.url)}
-                            >
+							>
 								<p
 									key={index}
 									className="px-10 text-xl font-bold transition-colors border-none"
@@ -40,16 +46,24 @@ const Navbar: React.FC<Props> = ({ links }): JSX.Element => {
 					})}
 				</div>
 				<div className="flex items-center justify-center">
-					<button 
-                        className="w-32 h-10 transition-colors bg-blue-500 rounded-2xl hover:bg-blue-700"
-                        onClick={() => navigate('/signin')}
-                    >
-						<p className="font-bold text-white">Sign in</p>
+					<button
+						className="w-24 h-10 transition-colors bg-blue-500 sm:w-32 rounded-2xl hover:bg-blue-700"
+						onClick={() => navigate("/signin")}
+					>
+						<div className="flex items-center justify-center">
+							<FaSignInAlt
+								className="inline-block mr-2 text-xl"
+								color="white"
+							/>
+							<p className="hidden font-bold text-white sm:inline-block">
+								Sign in
+							</p>
+						</div>
 					</button>
 				</div>
 			</div>
 		</nav>
 	);
-}
+};
 
 export default Navbar;
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,100 +1,172 @@
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
 import React, { useState, useEffect } from "react";
-import { FaEnvelope, FaLock, FaKey } from "react-icons/fa";
+import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
+import { FaEnvelope, FaLock, FaKey, FaCheck, FaTimes } from "react-icons/fa";
 import { HiAtSymbol } from "react-icons/hi";
 import Navbar from "../components/Navbar";
 import db from "../firebase";
 
+type StateSetter<T> = React.Dispatch<React.SetStateAction<T>>;
+
 const SignIn: React.FC = (): JSX.Element => {
+	const [isUser, setIsUser] = useState(false);
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
-	useEffect(() => {
-		console.log([username, email, password]);
-	}, [username, email, password]);
+	const passwordTests = {
+		0: p => !(p.length > 5),
+		1: p => !/[a-z]/.test(p),
+		2: p => !/[A-Z]/.test(p),
+		3: p => !/[1-9]/.test(p),
+		4: p => !(p === confirmPassword),
+	} as { [key: number]: (p: string) => boolean };
+	const testToName = {
+		0: "6 characters long",
+		1: "one lowercase letter",
+		2: "one uppercase letter",
+		3: "one number",
+		4: "passwords match",
+	} as { [key: number]: string };
+
 	return (
-		<div className="flex flex-col">
+		<>
 			<Navbar links={[{ name: "Home", url: "/" }]} />
-			<div className="min-h-[90vh] flex justify-center items-center grow-1 h-full w-full">
-				<div className="items-center justify-center h-auto p-6 bg-white rounded-lg shadow-2xl min-h-96 lg:w-[60%] xl:w-[35%]">
-					<h1 className="text-2xl font-bold text-center">Sign Up</h1>
+			<div className="min-h-[90vh] flex justify-center items-center h-full w-full">
+				<div className="items-center justify-center h-auto p-6 bg-white rounded-lg shadow-2xl min-h-96 w-[80%] xl:w-[40rem]">
+					<h1 className="text-2xl font-bold text-center">
+						{isUser ? "Sign In" : "Sign Up"}
+					</h1>
 					<br />
 					<form className="flex flex-col md:gap-10 md:flex-row">
-						<div className="flex flex-col items-center justify-center w-full">
-							<FieldInput
-								fieldName="username"
-								setter={setUsername}
-							/>
-							<FieldInput fieldName="email" setter={setEmail} />
-						</div>
-						<div className="flex flex-col items-center justify-center w-full">
-							<FieldInput
-								fieldName="password"
-								setter={setPassword}
-							/>
-							<FieldInput
-								fieldName="confirm password"
-								setter={setConfirmPassword}
-							/>
-						</div>
+						<Fields
+							isUser={isUser}
+							setUsername={setUsername}
+							setEmail={setEmail}
+							setPassword={setPassword}
+							setConfirmPassword={setConfirmPassword}
+							email={email}
+							password={password}
+							confirmPassword={confirmPassword}
+						/>
 					</form>
+					{!isUser && (
+						<div className="flex flex-col w-full gap-5 sm:gap-10 sm:flex-row">
+							<div className="w-full">
+								<h4 className="font-semibold">
+									Username requirements:
+								</h4>
+								<ul>
+									<div className="flex items-center">
+										{!(username.length >= 3) ? (
+											<FaTimes
+												color="red"
+												className="mr-1"
+											/>
+										) : (
+											<FaCheck
+												color="green"
+												className="mr-1"
+											/>
+										)}
+										<li className="text-sm text-gray-400">
+											At least 3 characters
+										</li>
+									</div>
+								</ul>
+								<br />
+								<h4 className="font-semibold">
+									Other requirements:
+								</h4>
+								<ul>
+									<div className="flex items-center">
+										{!/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+											email.toLowerCase()
+										) ? (
+											<FaTimes
+												color="red"
+												className="mr-1"
+											/>
+										) : (
+											<FaCheck
+												color="green"
+												className="mr-1"
+											/>
+										)}
+										<li className="text-sm text-gray-400">
+											Valid email
+										</li>
+									</div>
+								</ul>
+							</div>
+							<div className="w-full">
+								<h4 className="font-semibold">
+									Password requirements:
+								</h4>
+
+								<ul>
+									{[0, 1, 2, 3, 4].map(i => {
+										const res = passwordTests[i](password);
+										return (
+											<div className="flex items-center">
+												{res ? (
+													<FaTimes
+														color="red"
+														className="mr-1"
+													/>
+												) : (
+													<FaCheck
+														color="green"
+														className="mr-1"
+													/>
+												)}
+												<li className="text-sm text-gray-400">
+													{testToName[i]}
+												</li>
+											</div>
+										);
+									})}
+								</ul>
+							</div>
+						</div>
+					)}
 					<div className="flex flex-col items-center justify-center">
 						<div className="flex items-center justify-center w-full pb-5 border-b-2 border-gray-200">
 							<button
-								className="p-2 mt-4 text-white bg-blue-500 hover:bg-blue-700 transition-colors rounded-lg w-[60%] md:w-[30%]"
-								onClick={async e => {
-									e.preventDefault();
-									try {
-										if (
-											!username ||
-											!email ||
-											!password ||
-											!confirmPassword ||
-											password !== confirmPassword
-										) {
-											throw new Error("Invalid input");
-										}
-										const queryRes = await getDocs(
-											query(
-												collection(db, "user-data"),
-												where("email", "==", email)
-											)
-										);
-										if (queryRes.size > 0) {
-											throw new Error(
-												"Email already in use"
-											);
-										}
-										await addDoc(
-											collection(db, "user-data"),
-											{
-												username,
-												email,
-												password,
-											}
-										);
-									} catch (error) {
-										alert(error);
-									}
-								}}
+								className="w-32 p-2 mt-4 text-white transition-colors bg-blue-500 rounded-lg hover:bg-blue-700"
+								onClick={e =>
+									validateLogin(
+										e,
+										username,
+										email,
+										password,
+										confirmPassword
+									)
+								}
 							>
-								Sign Up
+								{isUser ? "Sign In" : "Sign Up"}
 							</button>
 						</div>
-						<p className="mt-4 text-gray-400 text-md">
-							Already have an account?{" "}
-							<a
-								href="/signin"
-								className="font-bold text-blue-500 hover:text-blue-700"
+						<p className="mt-4 text-center text-gray-400 text-md">
+							{isUser
+								? "Don't have an account? "
+								: "Already have an account? "}
+							<span
+								className="font-bold text-blue-500 cursor-pointer hover:text-blue-700"
+								onClick={() => {
+									setIsUser(prev => !prev);
+									setUsername("");
+									setEmail("");
+									setPassword("");
+									setConfirmPassword("");
+								}}
 							>
-								Sign In
-							</a>
+								{isUser ? " Sign Up" : " Sign In"}
+							</span>
 						</p>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 };
 
@@ -102,12 +174,18 @@ export default SignIn;
 
 interface IFieldInput {
 	fieldName: "username" | "email" | "password" | "confirm password";
-	setter: React.Dispatch<React.SetStateAction<string>>;
+	setter: StateSetter<string>;
+	email?: string;
+	password?: string;
+	confirmPassword?: string;
 }
 
 const FieldInput: React.FC<IFieldInput> = ({
 	fieldName,
 	setter,
+	email,
+	password,
+	confirmPassword,
 }): JSX.Element => {
 	const iconMap = {
 		username: <HiAtSymbol color="gray" />,
@@ -121,6 +199,7 @@ const FieldInput: React.FC<IFieldInput> = ({
 		password: "Secure password",
 		"confirm password": "Confirm password",
 	};
+
 	return (
 		<div className="w-full">
 			<label
@@ -132,18 +211,112 @@ const FieldInput: React.FC<IFieldInput> = ({
 					: fieldName.charAt(0).toUpperCase() + fieldName.slice(1)}
 			</label>
 
-			<div className="relative mb-6">
-				<span className="absolute inset-y-0 left-0 flex items-center px-3 pl-3 font-bold pointer-events-none">
-					{iconMap[fieldName]}
-				</span>
-				<input
-					type={fieldName === "password" ? "password" : "text"}
-					id={`${fieldName}group`}
-					className="input-form"
-					onChange={e => setter(e.target.value)}
-					placeholder={placeholderMap[fieldName]}
-				/>
+			<div className="flex w-full h-auto">
+				<div className="relative w-full mb-6">
+					<span className="absolute inset-y-0 left-0 flex items-center px-3 pl-3 font-bold pointer-events-none">
+						{iconMap[fieldName]}
+					</span>
+					<input
+						type={
+							fieldName === "password" ||
+							fieldName === "confirm password"
+								? "password"
+								: "text"
+						}
+						id={`${fieldName}group`}
+						className="w-full input-form"
+						onChange={e => setter(e.target.value)}
+						placeholder={placeholderMap[fieldName]}
+					/>
+				</div>
 			</div>
 		</div>
 	);
 };
+
+interface FieldsProps {
+	isUser: boolean;
+	setUsername: StateSetter<string>;
+	setEmail: StateSetter<string>;
+	setPassword: StateSetter<string>;
+	setConfirmPassword: StateSetter<string>;
+	email: string;
+	password: string;
+	confirmPassword: string;
+}
+
+const Fields: React.FC<FieldsProps> = ({
+	isUser,
+	setUsername,
+	setEmail,
+	setPassword,
+	setConfirmPassword,
+	email,
+	password,
+	confirmPassword,
+}): JSX.Element => {
+	if (isUser) {
+		return (
+			<>
+				<div className="flex flex-col items-center justify-center w-full">
+					<FieldInput fieldName="email" setter={setEmail} />
+					<FieldInput fieldName="password" setter={setPassword} />
+				</div>
+			</>
+		);
+	}
+	return (
+		<>
+			<div className="flex flex-col items-center justify-center w-full">
+				<FieldInput fieldName="username" setter={setUsername} />
+				<FieldInput fieldName="email" setter={setEmail} email={email} />
+			</div>
+			<div className="flex flex-col items-center justify-center w-full">
+				<FieldInput
+					fieldName="password"
+					setter={setPassword}
+					password={password}
+				/>
+				<FieldInput
+					fieldName="confirm password"
+					setter={setConfirmPassword}
+				/>
+			</div>
+		</>
+	);
+};
+
+async function validateLogin(
+	e: any,
+	username: string,
+	email: string,
+	password: string,
+	confirmPassword: string
+) {
+	e.preventDefault();
+	try {
+		if (
+			!username ||
+			!email ||
+			!password ||
+			!confirmPassword ||
+			password !== confirmPassword
+		) {
+			throw new Error("Invalid input");
+		}
+		const queryRes = await getDocs(
+			query(collection(db, "user-data"), where("email", "==", email))
+		);
+		if (queryRes.size > 0) {
+			throw new Error("Email already in use");
+		}
+		await addDoc(collection(db, "user-data"), {
+			username,
+			email,
+			password,
+		});
+		alert("success");
+	} catch (error) {
+		alert(error);
+	}
+}
