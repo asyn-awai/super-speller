e69a86a5448eb323fcb7688683742d7c5de377e5
async await <95399051+asyn-awai@users.noreply.github.com>
2022-10-19 18:25:20 +0000

    replit branch create

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,74 +5,78 @@ import "react-responsive-modal/styles.css";
 import { Modal } from "react-responsive-modal";
 
 interface NavbarItem {
-	name: string;
-	url: string;
+    name: string;
+    url: string;
 }
 
 interface Props {
-	children: React.ReactNode;
-	darkMode: boolean;
-	setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-	navbarProps?: NavbarItem[];
-	topNav?: boolean;
-	sideNav?: boolean;
-	footer?: boolean;
+    children: React.ReactNode;
+    darkMode: boolean;
+    setDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+    modalChildren?: React.ReactNode;
+    navbarProps?: NavbarItem[];
+    topNav?: boolean;
+    sideNav?: boolean;
+    footer?: boolean;
 }
 
 const Layout: React.FC<Props> = ({
-	children,
-	darkMode,
-	setDarkMode,
-	navbarProps = [],
-	topNav = false,
-	sideNav = false,
-	footer = false,
+    children,
+    darkMode,
+    setDarkMode,
+    modalChildren = null,
+    navbarProps = [],
+    topNav = false,
+    sideNav = false,
+    footer = false,
 }): JSX.Element => {
-	const [open, setOpen] = useState(false);
-	const [modalData, setModalData] = useState<React.ReactNode>(<></>);
-	return (
-		<>
-			<Modal open={open} onClose={() => setOpen(false)} center>
-				<h1>Modal</h1>
-			</Modal>
-			<div className="w-auto h-auto sm:h-full sm:w-full d">
-				{sideNav && (
-					<div className="fixed z-50 hidden w-24 transition-all duration-300 ease-in-out group hover:md:w-48 md:block">
-						<SideNav
-							darkMode={darkMode}
-							setDarkMode={setDarkMode}
-						/>
-					</div>
-				)}
-				{topNav && (
-					<TopNav
-						links={navbarProps}
-						darkMode={darkMode}
-						setDarkMode={setDarkMode}
-					/>
-				)}
-				<div
-					className={`${
-						sideNav && "md:ml-36"
-					} px-5 md:px-0 d md:mr-10`}
-				>
-					{children}
-				</div>
-				{footer && (
-					<>
-						{/* <br /> */}
-						<footer
-							className={`${
-								sideNav && "pl-32"
-							} transition-colors bg-gray-100 border border-none h-36 dark:bg-gray-700`}
-						>
-							Footer
-						</footer>
-					</>
-				)}
-			</div>
-		</>
-	);
+    const [open, setOpen] = useState(false);
+    // const [modalData, setModalData] = useState<React.ReactNode>(<></>);
+    return (
+        <>
+            {/*{modalChildren && (*/}
+                <Modal open={!!modalChildren && open} onClose={() => setOpen(false)} center>
+                    <div className="">
+                        {modalChildren}
+                    </div>
+                </Modal>
+            {/*)}*/}
+            <div className="w-auto h-auto sm:h-full sm:w-full d">
+                {sideNav && (
+                    <div className="fixed z-50 hidden w-24 transition-all duration-300 ease-in-out group hover:md:w-48 md:block">
+                        <SideNav
+                            darkMode={darkMode}
+                            setDarkMode={setDarkMode}
+                        />
+                    </div>
+                )}
+                {topNav && (
+                    <TopNav
+                        links={navbarProps}
+                        darkMode={darkMode}
+                        setDarkMode={setDarkMode}
+                    />
+                )}
+                <div
+                    className={`${sideNav && "md:ml-36"
+                        } px-5 md:px-0 d md:mr-10`}
+                >
+                    {children}
+                </div>
+                {footer && (
+                    <>
+                        {/* <br /> */}
+                        <footer
+                            className={`${sideNav && "pl-32"
+                                } transition-colors bg-gray-100 border border-none h-36 dark:bg-gray-700`}
+                        >
+                            Footer
+                        </footer>
+                    </>
+                )}
+            </div>
+        </>
+    );
 };
 
 export default Layout;
