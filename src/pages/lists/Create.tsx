57fa127637f891856030f0ece2b36e1cff4db25c
async await <95399051+asyn-awai@users.noreply.github.com>
2022-10-27 23:27:14 +0000

    create section add to list

--- src/pages/lists/Create.tsx
+++ src/pages/lists/Create.tsx
@@ -7,9 +7,8 @@ import Modal from "../../components/Modal";
 import { enableBodyScroll, disableBodyScroll } from "body-scroll-lock";
 
 interface Word {
-	index: number;
 	word: string;
-	description: string;
+	definition: string;
 }
 
 interface WordInfo {
@@ -25,11 +24,13 @@ interface Props {
 
 const Create: React.FC<Props> = ({ darkMode, setDarkMode }): JSX.Element => {
 	const [wordsList, setWordsList] = useState<Word[]>([]);
-	//everytime wordsList changes, update the index value in each word object
-	const updateWordsList = () => {
-		setWordsList(prev => prev.map((word, index) => ({ ...word, index })));
-	};
-
+    const [options, setOptions] = useState<{
+        hideWordInfo: boolean;
+    }>({
+        hideWordInfo: false
+    });
+    const [listTitle, setListTitle] = useState("");
+    const [listDescription, setListDescription] = useState("");
 	return (
 		<Layout darkMode={darkMode} setDarkMode={setDarkMode} sideNav>
 			{/* <br /> */}
@@ -86,8 +87,10 @@ const Create: React.FC<Props> = ({ darkMode, setDarkMode }): JSX.Element => {
 							</div>
 						</div>
 						<div className="w-3/4">
-							<WordCard />
-							<AddWordCard darkMode={darkMode} />
+                            {wordsList.map(({ word, definition }) => {
+                                return <WordCard word={word} definition={definition} setWordsList={setWordsList} key={nanoid()} />
+                            })}
+							<AddWordCard darkMode={darkMode} setWordsList={setWordsList} />
 						</div>
 					</div>
 				</div>
@@ -119,27 +122,30 @@ const Checkbox: React.FC<{ title: string }> = ({ title }) => {
 	);
 };
 
-const WordCard: React.FC<{ hideDef?: boolean }> = ({ hideDef = false }) => {
+const WordCard: React.FC<{
+    word: string,
+    definition: string,
+    setWordsList: React.Dispatch<React.SetStateAction<Word[]>>;
+    hideDef?: boolean
+}> = ({ word, definition, setWordsList, hideDef = false }) => {
 	return (
 		<div className="p-4 my-8 border border-gray-200 rounded-lg sm:p-6 lg:p-8 dark:border-gray-700 bg-white dark:bg-gray-700">
 			<div className="flex space-x-5">
 				<div className="flex items-center justify-center w-2/5 line-clamp-1">
-					<h3 className="font-bold text-xl">Word</h3>
+					<h3 className="font-bold text-xl">{word}</h3>
 				</div>
 				{!hideDef && (
 					<div className="w-3/5 h-auto break-words line-clamp-3">
-						aewefjaejfkhawekjfwekjawefhawehfjawehfkhawejhfjkawehjfhewkahfwjeafef
-						et dolore magna aliqua. Ut enim ad minim veniam, quis
-						nostrud exercitation ullamco laboris nisi ut aliquip ex
-						ea commodo consequat. Duis aute irure dolor in
-						reprehenderit in voluptate velit esse cillum dolore eu
-						fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-						non proident, sunt in culpa qui officia deserunt mollit
-						anim id est laborum.
+                        {definition}
 					</div>
 				)}
-				<div className="flex justify-end w-1/12 cursor-pointer">
-					<FaTimes size={25} color={"#3b82f6"} />
+				<div 
+                    className="flex justify-end w-1/12 cursor-pointer"
+					onClick={() => {
+                        setWordsList(prev => prev.filter(x => x.word !== word))
+                    }}
+                >
+                    <FaTimes size={25} color={"#3b82f6"} />
 				</div>
 			</div>
 		</div>
@@ -148,11 +154,13 @@ const WordCard: React.FC<{ hideDef?: boolean }> = ({ hideDef = false }) => {
 
 const AddWordCard: React.FC<{
 	darkMode: boolean;
-}> = ({ darkMode }): JSX.Element => {
+    setWordsList: React.Dispatch<React.SetStateAction<Word[]>>;
+}> = ({ darkMode, setWordsList }): JSX.Element => {
 	const [modalShow, setModalShow] = useState<boolean>(false);
-	const [currentWordInfo, setCurrentWordInfo] = useState<WordInfo[]>([]);
+	const [wordQueryInfo, setWordQueryInfo] = useState<WordInfo[]>([]);
 	const [wordInput, setWordInput] = useState<string>("");
 	const [defInput, setDefInput] = useState<string>("");
+    const wordInputRef = useRef<HTMLInputElement>(null);
 	const defInputRef = useRef<HTMLInputElement>(null);
 
 	useEffect(() => {
@@ -196,11 +204,23 @@ const AddWordCard: React.FC<{
 			}
 		}
 		if (resInfo.length > 1) {
-			setCurrentWordInfo(resInfo);
+			setWordQueryInfo(resInfo);
 			setModalShow(true);
 		}
 	};
 
+    const handleAddWord = () => {
+        if (wordInput.length === 0) return;
+        setWordsList(prev => [...prev, { word: wordInput.toLowerCase(), definition: defInput }])
+        setWordQueryInfo([]);
+        if (wordInputRef.current != null)
+            wordInputRef.current.value = "";
+        if (defInputRef.current != null)
+            defInputRef.current.value = "";
+        setWordInput("");
+        setDefInput("");
+    }
+    
 	return (
 		<>
 			<Modal
@@ -210,12 +230,13 @@ const AddWordCard: React.FC<{
 				title="Select a definition"
 			>
 				<div className="w-auto max-h-96 overflow-y-scroll p-3">
-					{currentWordInfo.map(info => (
+					{wordQueryInfo.map(info => (
 						<DefinitionCard
 							{...info}
 							key={nanoid()}
 							inputRef={defInputRef}
 							setModalShow={setModalShow}
+                            setDefInput={setDefInput}
 						/>
 					))}
 				</div>
@@ -232,20 +253,22 @@ const AddWordCard: React.FC<{
 						<div className="flex">
 							<input
 								id="word-input"
+                                ref={wordInputRef}
 								className="input-form-create dark:placeholder-gray-500 dark:bg-gray-800 w-full rounded-r-none"
 								onChange={e => setWordInput(e.target.value)}
 								placeholder="Word"
 							/>
 							<div
 								role="button"
-								className="text-sm h-10 w-56 bg-blue-500 hover:bg-blue-600 rounded-lg transition-colors flex items-center justify-center rounded-l-none"
-								onClick={handleGetDef}
+								className={`text-sm h-10 w-56 rounded-lg transition-colors flex items-center justify-center rounded-l-none select-none
+                                            ${wordInput.length === 0 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
+								onClick={() => { if (wordInput.length !== 0) handleGetDef() }}
 							>
 								<span className="mr-2">
 									<FaSearch size={15} color="white" />
 								</span>
 								<p className="md:block hidden text-white font-semibold">
-									Dictionary Lookup
+									Dictionary
 								</p>
 							</div>
 						</div>
@@ -262,13 +285,14 @@ const AddWordCard: React.FC<{
 							ref={defInputRef}
 							onChange={e => setDefInput(e.target.value)}
 							className="input-form-create dark:placeholder-gray-500 dark:bg-gray-800"
-							placeholder="Use the dictionary lookup button or type in your own definition"
+							placeholder="Use the dictionary lookup button or use your own"
 						/>
 						<div className="flex justify-end mt-5">
 							<div
 								role="button"
-								className="text-sm h-10 w-28 bg-blue-500 hover:bg-blue-600 rounded-lg transition-colors flex items-center justify-center"
-								onClick={() => {}}
+								className={`text-sm h-10 w-28 rounded-lg transition-colors flex items-center justify-center border border-opacity-0
+                                            ${wordInput.length === 0 ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-600"}`}
+                                onClick={handleAddWord}
 							>
 								<span className="mr-2">
 									<FaPlus size={15} color="white" />
@@ -291,12 +315,14 @@ const DefinitionCard: React.FC<{
 	partOfSpeech: string;
 	inputRef: React.RefObject<HTMLInputElement>;
 	setModalShow: React.Dispatch<React.SetStateAction<boolean>>;
+	setDefInput: React.Dispatch<React.SetStateAction<string>>;
 }> = ({
 	definition,
 	example,
 	partOfSpeech,
 	inputRef,
 	setModalShow,
+    setDefInput
 }): JSX.Element => {
 	const partOfSpeechAbv = {
 		noun: "n.",
@@ -313,6 +339,7 @@ const DefinitionCard: React.FC<{
 	const setText = () => {
 		if (inputRef.current === null) return;
 		inputRef.current.value = definition;
+        setDefInput(definition);
 		setModalShow(false);
 	};
 	return (
